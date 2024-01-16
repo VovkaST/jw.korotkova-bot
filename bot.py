f796cdce8a7b57816e918acf5704cc1cf6a0b8a7
Suntsov Vladimir <v.suncov@comita.digital>
2024-01-16 10:25:24 +0300

    Some button's text finalized

--- bot.py
+++ bot.py
@@ -5,26 +5,30 @@ import settings
 from const import Buttons, Messages
 
 bot = AsyncTeleBot(settings.TOKEN)
-main_menu = types.ReplyKeyboardMarkup(one_time_keyboard=False, resize_keyboard=True)
-main_menu.add(
-    types.KeyboardButton(Buttons.HELP.value),
+inlines = types.ReplyKeyboardMarkup(one_time_keyboard=False, resize_keyboard=True)
+inlines.add(
+    types.KeyboardButton(Buttons.ORDER.value),
+    types.KeyboardButton(Buttons.DELIVERY.value),
     types.KeyboardButton(Buttons.ABOUT.value),
 )
+main_menu = types.MenuButton()
 
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=["start", "restart"])
 async def handler_start(message: types.Message):
-    logger.info("Received start command [%s@%s]", message.from_user.id, message.from_user.username)
-    await bot.send_message(message.chat.id, Messages.START.value, reply_markup=main_menu)
+    logger.info("Received %s command [%s@%s]", message.text, message.from_user.id, message.from_user.username)
+    await bot.send_message(message.chat.id, Messages.START.value, reply_markup=inlines)
 
 
 @bot.message_handler(content_types=["text"])
 async def handler_messages(message: types.Message):
     logger.info("Received message: %s [%s@%s]", message.text, message.from_user.id, message.from_user.username)
-    if message.text == Buttons.HELP:
-        await bot.send_message(message.chat.id, Messages.HELP.value)
+    if message.text == Buttons.ORDER:
+        await bot.reply_to(message, Messages.ORDER.value)
     elif message.text == Buttons.ABOUT:
-        await bot.send_message(message.chat.id, Messages.ABOUT.value)
+        await bot.reply_to(message, Messages.ABOUT.value)
+    elif message.text == Buttons.DELIVERY:
+        await bot.reply_to(message, Messages.DELIVERY.value)
     else:
         logger.info("Unknown message: %s [%s@%s]", message.text, message.from_user.id, message.from_user.username)
-        await bot.send_message(message.chat.id, Messages.UNKNOWN.value)
+        await bot.reply_to(message, Messages.UNKNOWN.value)
